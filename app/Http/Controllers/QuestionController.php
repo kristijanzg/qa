36d6a341880b17dac98ec20c6a261beb330ad451
Kristijan <kiky.zg@gmail.com>
2018-12-02 17:08:26 +0100

    more changes

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -8,23 +8,19 @@ use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+    
     public function index()
     {
         $questions = Question::with('user')->latest()->paginate(5);
 
         return view('questions.index', compact('questions'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function create()
     {
         $question = new Question();
@@ -38,44 +34,33 @@ class QuestionController extends Controller
 
         return redirect('/questions')->with('success', 'Your question has been submited!');
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Question  $question
-     * @return \Illuminate\Http\Response
-     */
+    
     public function show(Question $question)
     {
-        //
-    }
+        $question->increment('views');
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Question  $question
-     * @return \Illuminate\Http\Response
-     */
+        return view('questions.show', compact('question'));
+    }
+    
     public function edit(Question $question)
     {
+        $this->authorize('update', $question);
         return view('questions.edit', compact('question'));
     }
 
     public function update(AskQuestionRequest $request, Question $question)
     {
+        $this->authorize('update', $question);
         $question->update($request->only(['title', 'body']));
 
         return redirect('/questions')->with('success', 'Your question has been updated');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Question  $question
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Question $question)
     {
-        //
+        $this->authorize('delete', $question);
+        $question->delete();
+
+        return redirect('/questions')->with('success', 'Your question has been deleted');
     }
 }
